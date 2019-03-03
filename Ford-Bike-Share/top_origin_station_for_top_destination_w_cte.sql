4e11ad559d7bfebfadc2bbdcc3ade9f5ddb762dd
Alpha-1787 <armand.kok@berkeley.edu>
2019-03-02 16:21:56 -0800

    Create top_origin_station_for_top_destination_w_cte.sql

---
+++ Ford-Bike-Share/top_origin_station_for_top_destination_w_cte.sql
@@ -0,0 +1,37 @@
+# getting top 5 origin station for each desitnation station with common table expression
+WITH top_destination_origin AS 
+	(SELECT
+		bt.end_station_name,
+		bt.start_station_name,
+		DENSE_RANK() OVER (PARTITION BY bt.end_station_name ORDER BY COUNT(*) DESC, bt.end_station_name) AS trip_count_rank,
+		COUNT(*) AS trip_count,
+		ROUND(AVG(bt.duration_sec)/60, 2) AS avg_trip_duration_min
+	FROM
+		`bigquery-public-data.san_francisco.bikeshare_trips` bt
+	GROUP BY
+		bt.start_station_name,
+		bt.end_station_name),
+top_destination AS
+	(SELECT
+		bt.end_station_name,
+		DENSE_RANK() OVER (ORDER BY COUNT(*) DESC, bt.end_station_name) AS trip_count_rank,
+		COUNT(*) AS trip_count,
+		ROUND(AVG(bt.duration_sec)/60, 2) AS avg_trip_duration_min
+	FROM
+		`bigquery-public-data.san_francisco.bikeshare_trips` bt
+	GROUP BY
+		bt.end_station_name)
+SELECT
+	tdo.end_station_name,
+	tdo.start_station_name,
+	tdo.trip_count,
+	tdo.avg_trip_duration_min
+FROM 
+	top_destination td
+INNER JOIN 
+	top_destination_origin tdo
+	ON 
+		tdo.end_station_name = td.end_station_name
+WHERE
+	td.trip_count_rank IN (1, 2, 3, 4, 5) 
+	AND tdo.trip_count_rank IN (1, 2, 3, 4, 5)
